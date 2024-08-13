38ce199182963d9fda209b09bc8e01cfadd9cf5f
Umut Utku ERŞAHİNCE <ersahinceumut@gmail.com>
2024-08-13 12:48:04 +0300

    edit on 2024-08-01

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722518874',resources: [
+    cacheName: 'chirpy-1723542461',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
